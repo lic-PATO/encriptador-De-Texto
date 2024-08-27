a3fe90de5aec2319c0d1a5dad7f25a18deaed037
Manuel <manuel.13.01.2000@gmail.com>
2024-08-27 18:46:34 -0500

    ultimos toques

--- encriptarTexto.js
+++ encriptarTexto.js
@@ -1,3 +1,6 @@
+let imagen = document.getElementById("munieco");
+let textosDerecha = document.getElementById("mensajesInicio");
+let areaTextDerecha = document.getElementById("textoFinal");
 
 
 function reemplazarVocales(parrafo1) {
@@ -13,11 +16,12 @@ function reemplazarVocales(parrafo1) {
             }
          }
     );
+    
+}
+function descartarAcentosMayusculas(parrafo1){
     const regex = /[A-ZÁÉÍÓÚáéíóú]/;
     if (regex.test(parrafo1)) {
-        return("Lo siento, puede que hayan MAYÚSCULAS o acentos.");
-    } else {
-        return texto.replace(/a/g, 'agua');
+        document.getElementById("textoFinal").innerText = "Lo siento, puede que hayan mayúsculas o acentos.";
     }
 }
 function elimiarDerecha(){
@@ -30,12 +34,71 @@ function elimiarDerecha(){
     }
 }
 function encripText(){
-    let textInicial = document.getElementById("areaEncriptar").value;
-
-    let textEncriptado = reemplazarVocales(textInicial);
-
-document.getElementById("textoFinal").innerText = textEncriptado;
-    
-elimiarDerecha;
+    var areaEncriptar = document.getElementById('areaEncriptar').value;
+    if(areaEncriptar === ""){
+        document.getElementById("textoFinal").innerText = "No hay texto por encriptar.";
+        elimiarDerecha();
+    } else{
+        let textInicial = document.getElementById("areaEncriptar").value;
+        let textEncriptado = reemplazarVocales(textInicial);
+        document.getElementById("textoFinal").innerText = textEncriptado;
+        
+        elimiarDerecha();
+        descartarAcentosMayusculas(textInicial);
+        mostrarBoton();
+    }
 }
 
+function reemplazarFraces(parrafo2) {
+    const reemplazosInversos = {
+        'ai': 'a',
+        'enter': 'e',
+        'imes': 'i',
+        'ober': 'o',
+        'ufat': 'u'
+    };
+
+    for (var clave in reemplazosInversos) {
+        var valor = reemplazosInversos[clave];
+        parrafo2 = parrafo2.replaceAll(clave, valor);
+    }
+
+    return parrafo2;
+}
+function desenpripText(){
+    var areaEncriptar = document.getElementById('areaEncriptar').value;
+    if(areaEncriptar === ""){
+        document.getElementById("textoFinal").innerText = "No hay texto por desencriptar.";
+        elimiarDerecha();
+    } else{
+        let textInicial = document.getElementById("areaEncriptar").value;
+
+    let textDesencriptado = reemplazarFraces(textInicial);
+document.getElementById("textoFinal").innerText = textDesencriptado;
+
+elimiarDerecha();
+descartarAcentosMayusculas(textInicial);
+mostrarBoton();
+    }
+    
+}
+function mostrarBoton() {
+    var botonCopiar = document.getElementById("botonCopiar");
+    botonCopiar.style.display = 'block'; 
+}
+
+function copiarTexto() {
+    var textoCopiado = document.getElementById("textoFinal");
+    var rango = document.createRange();
+    rango.selectNodeContents(textoCopiado);
+    var seleccion = window.getSelection();
+    seleccion.removeAllRanges();
+    seleccion.addRange(rango);
+    try {
+        document.execCommand("copy");
+        console.log("Texto copiado al portapapeles.");
+    } catch (err) {
+        alert("Hubo un problema al copiar el texto.");
+    }
+    seleccion.removeAllRanges();
+}
